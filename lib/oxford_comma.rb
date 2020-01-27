bd1ff0e8aafb4ce849ea2f4e2d88d9b0f6fcd513
taroj khan <tarujkhan@yahoo.com>
2020-01-27 21:32:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,25 @@
+require 'pry'
+
 def oxford_comma(array)
-array = []
-"#{array.join(",")}"
-return array
+
+# You have to write a conditional statement (if else)
+
+# if array is length of one, convert array to string and return string
+# However if array is two elements- add an and, convert to string, return string
+# if array is 3 elements or more 
+#binding.pry
+if array.length == 1 
+return array.join(", ")
+elsif array.length == 2 
+   return array.join(" and ")
+
 end
+
+
+
+#new_array = []
+array[-1].insert(0,"and ")
+#new_array.join(", ")
+#binding.pry
+return array.join(", ")
 end
