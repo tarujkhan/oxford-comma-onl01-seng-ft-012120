72c7696afff8e7a0187e526806ddfe91c87b0018
taroj khan <tarujkhan@yahoo.com>
2020-01-27 19:08:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,13 @@ require 'pry'
 
 def oxford_comma(array)
 
+# You have to write a conditional statement (if else)
+
+# if array is length of one, convert array to string and return string
+
+
+
+
 #new_array = []
 new_array = array.insert(-2, "and")
 new_array.join(", ")
