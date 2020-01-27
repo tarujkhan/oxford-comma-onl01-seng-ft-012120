df1142a243b0da1eb621ff7a3cd784a9e9bc044d
taroj khan <tarujkhan@yahoo.com>
2020-01-27 19:09:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,8 @@ def oxford_comma(array)
 # You have to write a conditional statement (if else)
 
 # if array is length of one, convert array to string and return string
+# However if array is two elements- add an and, convert to string, return string
+# if array is 3 elements or more 
 
 
 
