d421154cdfc8636545195a9039244ce704d6f1ce
taroj khan <tarujkhan@yahoo.com>
2020-01-27 20:23:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,11 +12,10 @@ if array.length = 1
   return array 
 elsif array.length = 2 
     array.insert("and")
-    return array
 else array.length = 3
     array.insert(", ")
-    return array
   end
+  return array
 end
 
 
