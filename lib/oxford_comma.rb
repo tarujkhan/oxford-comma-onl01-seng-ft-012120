fba535fe43e1512d94a146cc4fd312384a5e8b36
taroj khan <tarujkhan@yahoo.com>
2020-01-27 20:21:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,8 +13,7 @@ if array.length = 1
 elsif array.length = 2 
     array.insert("and")
     return array
-else
-  array.length = 3
+else array.length = 3
     array.insert(", ")
     return array
   end
