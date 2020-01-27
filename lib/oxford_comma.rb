86898d3c814879e0411d58c6dcb18614df339141
taroj khan <tarujkhan@yahoo.com>
2020-01-27 19:15:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,11 +8,15 @@ def oxford_comma(array)
 # However if array is two elements- add an and, convert to string, return string
 # if array is 3 elements or more 
 
-if array is length of one 
-  
+if array.length = 1 
+  return array 
 elsif 
-  
+  if array.length = 2 
+    array.insert("and")
+    return array
 else
+  if array.length = 3
+    array.
   
 end
 
