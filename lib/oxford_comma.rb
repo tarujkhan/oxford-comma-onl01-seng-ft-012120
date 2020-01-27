ac3546ba29c314d938a3a1be2afd7c356c6463dd
taroj khan <tarujkhan@yahoo.com>
2020-01-27 21:05:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,7 @@ binding.pry
 if array.length == 1 
   array.join(", ")
 elsif array.length == 2 
-    array.join("and")
+    array.join(" and ")
 #else array.length > 3
  #   array.insert(", ")
   #end
